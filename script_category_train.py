53157b0b936f200c6f5ec23ef40df65b1a89e2da
JuneBS <june.sallou@laposte.net>
2016-11-04 21:28:50 +0100

    script updated - extracting entities and related categories from training data

--- script_category_train.py
+++ script_category_train.py
@@ -1,16 +1,36 @@
-import re, glob
-dirname=''
-d_entities={}
-p=re.compile (r'.+\t(.+)\t\d+-\d+\s(.+)')
+#!/usr/bin/python3.5
+#coding: utf-8
 
-files=glob.glob(dirname+'*.ent')
-for file in files:
-    with open(file, 'r') as fichier:
-        for line in fichier:
-            m=p.match(line)
-            if m:
-                cat = m.group(1)
-                nom = m.group(2)
-            if nom not in d_entities:
-                d_entities[nom]=cat
-print(d_entities)
+import re, glob
+
+
+def extract_entities_cat(dirname):
+	"""
+		
+
+
+	"""
+	d_entities={}
+	p=re.compile(r'\d+\t([A-Za-z]+)\s\d+-\d+\s([A-Za-z ]+)(\s\d+-\d+\s([A-Za-z ]+))?')
+
+	files=glob.glob(dirname+'*.ent')
+	for file in files:
+    		with open(file, 'r') as fichier:
+        		for line in fichier:
+            			m=p.match(line)
+            			if not m: break; 
+            			cat = m.group(1)
+            			nom = m.group(2)
+            			if nom not in d_entities:
+                			d_entities[nom]=cat
+            			if m.group(4) is not None and m.group(4) not in d_entities:
+                			d_entities[m.group(4)]=cat
+                
+	return d_entities
+
+
+
+dirname='/media/DATA/Master/Analyse_donnees_text/data_suj1/train/'
+
+dico_entities_cat=extract_entities_cat(dirname)
+print(dico_entities_cat)
