840734a188f589ab345ad6339c0e915c2d92ca0b
JuneBS <june.sallou@laposte.net>
2016-11-17 21:12:30 +0100

    docstring added to the function extract_entities_cat()

--- script_category_train.py
+++ script_category_train.py
@@ -6,8 +6,13 @@ import re, glob
 
 def extract_entities_cat(dirname):
 	"""
-		
+		Extracts the entities and according categories from the .ent files in a training directory
+		and returns a dictionnary of all the entities as keys and their categories as values.
 
+		:param dirname: name of the directory where the files are located
+		:type dirname: str
+		:return: entities and categories of the .ent files in the training directory
+		:rtype: dict of str
 
 	"""
 	d_entities={}
@@ -21,7 +26,7 @@ def extract_entities_cat(dirname):
             			if not m: break
             			cat = m.group(1)
             			nom = m.group(2)
-						if cat == 'Bacteria': continue
+				if cat == 'Bacteria': continue
             			if nom not in d_entities:
                 			d_entities[nom]=cat
             			if m.group(4) is not None and m.group(4) not in d_entities:
